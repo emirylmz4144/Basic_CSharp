fdadb750774e3caba861c258a8cac63618492ac7
Emir Yılmaz <emiryilmaz4144@gmail.com>
2023-01-27 11:34:47 +0300

    files are changed

---
+++ MigrotaryBirds/MigrotaryBirds/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Migrotary
+{
+    class MigrotaryBirds
+    {
+        static void Main(string[] args)
+        {
+            int count = Convert.ToInt32(Console.ReadLine());
+            int[] ourArray = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                //Kullanıcıdan alınan değerler diziye aktarılıyor
+                int value = Convert.ToInt32(Console.ReadLine());
+                ourArray[i] = value;
+            }
+
+            int[] mikrotaryBird = mikrotaryCounter(ourArray);//İlgili bilgiler ilgili fonksiyondadır.
+
+            int maxMikrotaryCounter = 1;//en fazla tekrar eden elemanın tekrar sayısınınn atandığı değişken
+            int mikrotaryCounterValue = ourArray[0];// en fazla tekrar eden elemanın atandığı değişken
+
+
+            for (int i = 0; i < mikrotaryBird.Length; i++)
+            {
+                if (mikrotaryBird[i] > maxMikrotaryCounter)//ilgili indisdeki elemanın ne kadar tekrar ettiğini karşılaştırıyoruz
+                {
+                    maxMikrotaryCounter = mikrotaryBird[i];// Şart sağlandıysa tekrar sayısını yeni max tekrar eden miktarı yapıyoruz
+                    mikrotaryCounterValue = ourArray[i];// ve en fazla tekrar eden elemanı ilgili değişkene atıyoruz
+                }
+                else if (mikrotaryBird[i] == maxMikrotaryCounter && ourArray[i] < mikrotaryCounterValue)
+                {
+                    /*
+                    *Burası çok önemlidir çünki en dizinin ilgiili değişkeni en fazla tekrar etme miktarı ile aynı olabilir
+                    * bu durumda en küçük değeri en fazla tekrar eden eleman diye alacağımız için bu şart bizim kilit noktamızdır
+                     */
+                    mikrotaryCounterValue = ourArray[i];
+                }
+            }
+
+
+            Console.WriteLine(mikrotaryCounterValue);
+        }
+
+
+
+
+
+        public static int[] mikrotaryCounter(int[] ourArray)
+        {
+            int[] migrotaryBird = new int[ourArray.Length];
+            for (int i = 0; i < ourArray.Length; i++)
+            {
+                for (int j = 0; j < ourArray.Length; j++)
+                {
+                    if (isFind(ourArray[i], ourArray, i))//İlgili metotta anlatılmıştır
+                    {
+                        continue;//İlgili metot sağlanıyorsa dizinin ilgili değişkeni es geçilir ve tekrar etme sayısı 0 atanır
+                    }
+
+                    if (ourArray[i] == ourArray[j])
+                    {
+                        /*
+                         *dizideki ilgili indisdeki değer daha önce sayaca eklenmediysse ve yine tekrar edildiyse sayaç artar
+                         */
+                        migrotaryBird[i]++;
+
+                    }
+                }
+            }
+            return migrotaryBird;
+        }
+
+
+
+
+
+
+
+        public static bool isFind(int value, int [] array, int tempCounter)
+        {
+            /*
+             *Bu kısımda kilit noktalar arasınd yer almaktaddır dizinin i.elemanı dizi ve i sayısı gönderilir
+             *ve for döngüsü i sayısını almadan burası çok önemli ^^İ SAYISINI ALMADAN^^ o sayıya kadar olan
+             * indisdeki değişkenleri dizinin i.elemanı ile kontrol eder ve eğer eşitlerse zaten o dizi sayaca
+             * eklenmiştir demektir true değer döndererek ilgili indisin zaten daha önce sayaca dahil edildiğini
+             * belirtir ve böylece sayaca dahil olmaz..
+             */
+            for (int i = 0; i < tempCounter; i++)
+            {
+                if (value == array[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+    }
+}
---
+++ ReferenceUse/ReferenceUse/refUse.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace refUse
+{
+    class useToRef
+    {
+        static void Main(string[]args)
+        {
+
+            int x = 6;
+            int y = 7;
+
+            deneme(x, y);
+            Console.WriteLine("x: " + x + "y: " + y); // x: 6 y:7
+            deneme2(ref x, ref y);
+            Console.WriteLine("x: " + x + "y: " + y); // x:10 y :11
+        }
+        static void deneme(int x, int y)
+        {
+            x = x + 4; y = y + 4;
+        }
+        static void deneme2(ref int x, ref int y)
+        {
+            x = x + 4; y = y + 4;
+        }
+    }
+}
