3becefae4477f4e2a8e52aef6d03ec2f03d7d605
Emir Yılmaz <emiryilmaz4144@gmail.com>
2023-01-31 15:16:13 +0300

    generic method project created

---
+++ GenericMethod/GenericMethod/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace ourProgress
+{
+    class Printer
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen kaç elemanlı dizi gireceğinizi yazınız: ");
+            
+            int count = Convert.ToInt32(Console.ReadLine());
+            int[] intArray = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Lütfen dizinin "+(i+1)+".elemanını giriniz: ");
+                intArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            count = Convert.ToInt32(Console.ReadLine());
+            string[] stringArray = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Lütfen dizinin " + (i + 1) + ".elemanını giriniz: ");
+                stringArray[i] = Console.ReadLine();
+            }
+
+            PrintArray<String>(stringArray);
+            PrintArray<Int32>(intArray);
+
+        }
+
+        static void PrintArray<T>(T[] ourArray)
+        {
+            foreach (T t in ourArray)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
+
+
+
+
