11041d6e3686c8d447b6b96776f7963171873a33
Emir Yılmaz <emiryilmaz4144@gmail.com>
2023-03-04 20:46:16 +0300

    Binary Addition is added

---
+++ BinaryAddition/BinaryAddition/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace BinaryAddition
+{
+    public class Addition
+    {
+        static void Main(string[] args)
+        {
+            List<int> ourList=new List<int>();
+            Console.WriteLine("<C>ıkıs yapana kadar sayı giriniz: ");
+            while(true)
+            {
+                var value = Console.ReadLine();
+                int ourİntValue;
+                if(int.TryParse(value,out ourİntValue))
+                {
+                    ourList.Add(ourİntValue);
+                }
+                else
+                {
+                    if (value.Equals("c") || value.Equals("C"))
+                        break;
+                    else
+                        Console.WriteLine("Lütfen geçeerli bir işlem yapınız:");
+                }
+            }
+
+            for(int i=0;i<ourList.Count;i+=2)
+            {
+                Console.WriteLine((ourList[i] + ourList[i+1]));
+            }
+            Console.ReadKey();
+        }
+    }
+}
