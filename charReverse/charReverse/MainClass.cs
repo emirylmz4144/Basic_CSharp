98889789b8b38208ab176d5eb83875e47275f86e
Emir Yılmaz <emiryilmaz4144@gmail.com>
2023-03-05 18:04:48 +0300

    char reversed is added

---
+++ charReverse/charReverse/MainClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chaReverse
+{
+    public static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Reverse.ReverseToString("Merhabalar efendim benim adım emir yılmaz"));
+        }
+    }
+}
---
+++ charReverse/charReverse/Reverse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace chaReverse
+{
+    public static class Reverse
+    {
+        public static string ReverseToString(string ourString)
+        {
+            string [] list = ourString.Split(" ");  
+            string newString = "";
+            
+
+            for(int i=0;i<list.Length;i++)
+            {
+                
+                char[] chars= list[i].ToCharArray();
+
+                char tempChar= chars[0];//Değişim yapılacağı için ilk harf kaybolmasın diye geçici bir şekilde tutulur
+
+                // Kelimelerin ilk ve son karakterleri yer değiştirir
+                chars[0]= chars[chars.Length-1];
+                chars[chars.Length - 1] = tempChar;
+
+                string updatedWord = new string(chars);// dizi şeklinde olan kelimemiz string değere aktarılır
+                newString += updatedWord+" ";
+
+            }
+            
+            return newString;
+        }
+    }
+}
