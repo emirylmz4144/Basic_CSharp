feecd1bd94d861d7dea0722f6df438d562eb3f8a
Emir Yılmaz <emiryilmaz4144@gmail.com>
2023-03-09 19:44:49 +0300

    CononantsNextTo project is added

---
+++ ConsonantsNextTo/ConsonantsNextTo/ConvertorClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace ConsonantsNextTo
+{
+    public static class ConvertorClass
+    {
+        public static bool[] returnTheStatus(string[] arrayText)
+        {
+            bool[] allWordsStatus = new bool[arrayText.Length];
+            char[] ourConstChars = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };//Bu harflerin dışında olanlar sessiz harflerdir
+
+            for (int i = 0; i < arrayText.Length; i++)
+            {
+                string word = arrayText[i];
+                for (int j = 0; j < word.Length-1; j++)
+                {
+                    if (!ourConstChars.Contains(word[j]) && !ourConstChars.Contains(word[j+1]))//Eğer harflerin dışındakileri 2 eleman içeriyorsa o zaman sessiler yanyanadır
+                    {
+                        {
+                            allWordsStatus[i] = true;
+                            break;
+                        }
+                    }
+                    
+                    
+                }
+            }
+
+            return allWordsStatus;
+        }
+    }
+}
---
+++ ConsonantsNextTo/ConsonantsNextTo/MainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsonantsNextTo
+
+{
+    public class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen bir metin giriniz: ");
+            string text= Console.ReadLine();
+
+            bool[] allWordsStatus = ConvertorClass.returnTheStatus(text.Split(" "));//metni kelimelere ayırırız
+
+            for (int i = 0; i < allWordsStatus.Length; i++)
+            {
+                Console.Write(allWordsStatus[i]+" ");
+            }
+
+        }
+    }
+}
+
